a65e31bc3795e3d57bd392124b6d740a8112ef3c
Sakshi Gauro <gaurosakshi113@gmail.com>
2021-08-12 01:11:05 -0400

    Calculations

---
+++ src/main/java/com/variables/calculations/Calculations.java
@@ -0,0 +1,11 @@
+package com.variables.calculations;
+
+public class Calculations {
+    public static void main(String[] args) {
+        double personHeight = 1.6;
+        double flagHeight = 4.3;
+
+        double totalHeight = (personHeight * 7) + flagHeight;
+        System.out.println(totalHeight);
+    }
+}
---
+++ src/main/java/com/variables/calculations/ReadMe.md
@@ -0,0 +1,3 @@
+# Calculations
+- In Java convention, variables are written in lowerCamelCase. 
+- The variable starts with a lower casr and each letter after that, the first letter is in Upper case (Eg: `totalResultsOfHeight`)
