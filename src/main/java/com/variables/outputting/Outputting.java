f5fbbb6c8756ee4801664fc1e2ce9d3eacd11288
Sakshi Gauro <gaurosakshi113@gmail.com>
2021-08-12 00:28:22 -0400

    Outputting

---
+++ src/main/java/com/variables/outputting/Outputting.java
@@ -0,0 +1,13 @@
+package com.variables.outputting;
+
+public class Outputting {
+    public static void main(String[] args) {
+        int cats = 10;
+        int dogs = 11;
+        int rhino = 14400;
+        int total = cats + dogs + rhino;
+
+        System.out.println("Number of cats: "+ cats);
+        System.out.println("Total number of animals: " + total);
+    }
+}
---
+++ src/main/java/com/variables/outputting/ReadMe.md
@@ -0,0 +1,5 @@
+# Outputting text and variables
+```java
+System.out.println("Total number of animals" + total);
+```
+- concatenating the text and the value of `total`. 
